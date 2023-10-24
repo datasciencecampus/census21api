e818b8a551ca6d8519cf02cff80f22cf9e63fe93
Ed Cuss <ed.cuss@ons.gov.uk>
2023-10-24 16:09:09 +0100

    fix replace commas with underscores in query

--- Census21_CACD_Wrapper.py
+++ Census21_CACD_Wrapper.py
@@ -320,4 +320,4 @@ class APIWrapper:
         
         for combination in no_single_searches:
             stripped_string = ','.join(combination)
-            data = self.query_api(residence_code, f"{stripped_string.replace(',','_')}", region)
+            data = self.query_api(residence_code, stripped_string, region)
