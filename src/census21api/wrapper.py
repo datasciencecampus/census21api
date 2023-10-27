7fc45833f8fbbc35b6d04f0c62a5c92ba0e8a3ac
Henry Wilde <henrydavidwilde@gmail.com>
2023-10-27 14:45:57 +0100

    Record API constants (#34)
    * Write a test for creating an instance
    
    * Create a file for the constants to live in
    
    * Remove mention of constant data from API
    
    * Add a workflow to check the constants every week
    
    * Format wrapper module
    
    * Format constants checker
    
    * Add documentation for logger parameter

--- src/census21api/wrapper.py
+++ src/census21api/wrapper.py
@@ -1,33 +1,40 @@
+"""Module for the API wrapper."""
+
 import itertools
 import json
 import os
-from typing import Any, Dict, List, Optional, Set
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 import requests
 from requests.models import Response
 
+from census21api.constants import API_ROOT, DIMENSIONS_BY_POPULATION_TYPE
 
-class APIWrapper:
+
+class CensusAPI:
+    """
+    A wrapper for the 2021 England and Wales Census API.
+
+    Parameters
+    ----------
+    logger : bool, default False
+        Whether to be verbose about issues when connecting to the API.
+
+    Attributes
+    ----------
+    _current_data : dict or None
+        Data dictionary from the most recent API call. If no call has
+        been made or the last call failed, this is `None`.
+    _current_url : str or None
+        URL of the most recent API call. If no call has been made, this
+        is `None`.
+    """
+
     def __init__(self, logger: bool = False) -> None:
-        self._logger = logger
-        self._current_data = None
-        self.set_root_string("https://api.beta.ons.gov.uk/v1/population-types")
-        self.set_population_types_available()
-        self.set_areas_by_pop_types()
-        self.set_dims_by_pop_types()
-
-    def set_root_string(self, root_str: str) -> None:
-        """
-        set the root string for the API
-        """
-        self._root_string = root_str
-
-    def get_root_string(self) -> str:
-        """
-        getter function for the root string
-        """
-        return self._root_string
+        self._logger: bool = logger
+        self._current_url: str = None
+        self._current_data: dict = None
 
     def _validate_status_code(self, response: Response) -> bool:
         """
@@ -90,120 +97,6 @@ class APIWrapper:
         self._current_data = self._process_reponse(response)
         return self._current_data
 
-    def extract_pop_type_from_item(self, item: Dict[str, str]) -> str:
-        """
-        helper function for splitting pop type
-        """
-        return item["name"].split("-")[3].upper()
-
-    def set_population_types_available(self) -> List[str]:
-        """
-        Loops through the possible codes to see if returns data.
-        Saves successful codes to a list
-
-        Returns:
-            list of the successful population codes.
-        """
-
-        root_string: str = self.get_root_string()
-        self.get(root_string)
-
-        # list comprehension, splits out the suspected population codes from the longer call_code response
-        # converts to upper case to mirror upper case shown in the github demo found here:
-        # https://github.com/GSS-Cogs/knowledge/blob/main/api/ons_local_census_api_walkthrough_python.ipynb
-        # converts to set to remove duplicates and back to list for easy manipulation
-        possible_codes: Set[str] = set(
-            [
-                self.extract_pop_type_from_item(item)
-                for item in self._current_data["items"]
-            ]
-        )
-        working_pop_codes: List[str] = []
-
-        for cd in possible_codes:
-            search_string = f"{root_string}/{cd}"
-            self.get(search_string)
-
-            # if the attempt returns none we know get has failed
-            # sometimes the API returns a json with "errors" as a key
-            # that also counts as failing so we ignore that
-            if (
-                isinstance(self._current_data, dict)
-                and "errors" not in self._current_data.keys()
-            ):
-                working_pop_codes.append(cd)
-
-        self._valid_pop_types = working_pop_codes
-
-    def get_population_types_available(self) -> List[str]:
-        return self._valid_pop_types
-
-    def get_parameters_available(
-        self, pop_type: str, parameter_type: str
-    ) -> Dict[str, str]:
-        """
-        get the available parameters for either area-type or dimensions
-
-        Args:
-            pop_type: str = UR, HRP, HH
-            parameter_type: str = area-types, dimensions
-
-        Returns:
-            dictionary of descriptive name and relevant id.
-        """
-        # the API structure of parameter_type coming after population type means we can use the same \
-        # function for two calls, if this changes at some point this function will break
-        # TODO add some error checking for the pop and parameter type inputs
-        self.get(f"{self.get_root_string()}/{pop_type}/{parameter_type}")
-
-        # dictionary comprehension to connect the description with the useful id code
-        if isinstance(self._current_data["items"], list):
-            possible_params = {
-                item["label"]: item["id"]
-                for item in self._current_data["items"]
-            }
-            return possible_params
-        if self._logger:
-            print(
-                f"Unable to iterate over NoneType for {pop_type = } and {parameter_type = }"
-            )
-
-    def set_areas_by_pop_types(self) -> None:
-        """
-        Loops over the valid pop types and creates a dict of valid area types for each of them
-        """
-        self._area_dict: Dict[str, str] = {}
-
-        for pt in self._valid_pop_types:
-            self._area_dict[pt] = self.get_parameters_available(
-                pt, "area-types"
-            )
-
-    def set_dims_by_pop_types(self) -> None:
-        """
-        Loops over the valid pop types and creates a dict of valid dimensions for each of them
-        """
-        self._dims_dict: Dict[str, str] = {}
-
-        for pt in self._valid_pop_types:
-            self._dims_dict[pt] = self.get_parameters_available(
-                pt, "dimensions"
-            )
-
-    def get_areas_by_pop_type(self, pop_type: str) -> Dict[str, str]:
-        """
-        Returns:
-            a dictionary of available area codes for the input arg pop_type
-        """
-        return self._area_dict[pop_type]
-
-    def get_dims_by_pop_type(self, pop_type: str) -> Dict[str, str]:
-        """
-        Returns:
-            a dictionary of available dimensions for the input arg pop_type
-        """
-        return self._dims_dict[pop_type]
-
     def build_search_string(
         self,
         search_pop_type: str,
@@ -222,9 +115,7 @@ class APIWrapper:
             the url for the API call
         """
         # base string to build search off
-        search_string: str = (
-            f"{self.get_root_string()}/{search_pop_type}/census-observations?"
-        )
+        search_string = f"{API_ROOT}/{search_pop_type}/census-observations?"
 
         parameter_strings = (
             f"{name}={param}"
@@ -371,7 +262,7 @@ class APIWrapper:
         Returns:
             .csv file for all possible combinations
         """
-        dims = list(self.get_dims_by_pop_type(residence_code).values())
+        dims = list(DIMENSIONS_BY_POPULATION_TYPE[residence_code].values())
         combos = [sorted(i) for i in itertools.product(dims, dims)]
         trimmed_combos = sorted(list(map(list, set(map(frozenset, combos)))))
         no_single_searches = [i for i in trimmed_combos if len(i) == 2]
