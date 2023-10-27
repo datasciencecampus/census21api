49f7c4e1707fc0a2315ca72b9855dcfec96a3646
Henry Wilde <henrydavidwilde@gmail.com>
2023-10-27 13:42:30 +0100

    Create a file for the constants to live in

--- src/census21api/__init__.py
+++ src/census21api/__init__.py
@@ -1,6 +1,7 @@
 """A Python wrapper for the England and Wales Census 2021 API."""
 
+from . import constants
 from .interface import Interface
-from .wrapper import APIWrapper
+from .wrapper import CensusAPI
 
-__all__ = ["APIWrapper", "Interface"]
+__all__ = ["CensusAPI", "Interface", "constants"]
