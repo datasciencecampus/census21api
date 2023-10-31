dc6f942d0b6161d61bee5c90fdc2b79b08af3645
Henry Wilde <henrydavidwilde@gmail.com>
2023-10-31 15:58:07 +0000

    Remove interface from __init__

--- src/census21api/__init__.py
+++ src/census21api/__init__.py
@@ -1,7 +1,6 @@
 """A Python wrapper for the England and Wales Census 2021 API."""
 
 from . import constants
-from .interface import Interface
 from .wrapper import CensusAPI
 
-__all__ = ["CensusAPI", "Interface", "constants"]
+__all__ = ["CensusAPI", "constants"]
