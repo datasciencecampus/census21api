7c3337efd62a971ee3d384fc2c80471807298600
Henry Wilde <henrydavidwilde@gmail.com>
2024-02-16 12:57:02 +0000

    Add original #39 test back in (+ assure it fails)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,6 +5,29 @@ import pytest
 from census21api import CensusAPI
 
 
+def test_issue_39_blocked_pairs():
+    """
+    https://github.com/datasciencecampus/census21api/issues/39
+
+    This test no longer works as is. See
+    `test_issue_39_blocked_pairs_gets_400_error` for an update to this
+    case. However, we keep this test for posterity, and test that it
+    **doesn't** work.
+    """
+
+    population_type = "UR_HH"
+    area_type = "nat"
+    dimensions = ("economic_activity_status_12a", "industry_current_88a")
+
+    api = CensusAPI()
+
+    with pytest.warns(UserWarning) as w:
+        assert "blocked pair" not in w
+        table = api.query_table(population_type, area_type, dimensions)
+
+    assert table is None
+
+
 def test_issue_39_blocked_pairs_gets_400_error():
     """
     Originally, this was about catching blocked pairs.
